ae1141e02c83784cd4f346319d09d3c29bed7947
SanketTops <sanket.tops@gmail.com>
2023-11-07 10:42:04 +0530

    Loop + Control Statements

---
+++ Module-1/for_example.py
@@ -0,0 +1,20 @@
+"""for i in "Python":
+    print(i)"""
+
+"""for i in range(1,11):
+    print(i)"""
+
+"""for i in range(1,11,2):
+    print(i)"""
+
+
+"""
+for i in range(10,0,-1):
+    print(i)"""
+
+"""for i in range(2,11,2):
+    print(i)"""
+
+n=5
+for i in range(1,11):
+    print(f"{n}*{i} = {n*i}")
---
+++ Module-1/pattern1.py
@@ -0,0 +1,29 @@
+"""for r in range(1,6):
+    for c in range(r):
+        print("*",end=" ")
+    print(" ")"""
+
+"""for r in range(1,6):
+    for c in range(r):
+        print(c+1,end=" ")
+    print(" ")"""
+
+"""for r in range(1,6):
+    for c in range(r):
+        print(r,end=" ")
+    print(" ")"""
+
+"""for r in range(5,0,-1):
+    for c in range(r):
+        print("*",end=" ")
+    print(" ")"""
+
+"""for r in range(5,0,-1):
+    for c in range(r):
+        print(r,end=" ")
+    print(" ")"""
+
+for r in range(5,0,-1):
+    for c in range(r):
+        print(c+1,end=" ")
+    print(" ")
