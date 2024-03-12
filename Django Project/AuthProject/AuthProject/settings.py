088bf636b2085b2665449cf042c6cc434a00d55a
SanketTops <sanket.tops@gmail.com>
2024-03-12 10:35:35 +0530

    Session

--- Django Project/AuthProject/AuthProject/settings.py
+++ Django Project/AuthProject/AuthProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'AuthProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "newauth",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
--- Django Project/AuthProject/myapp/views.py
+++ Django Project/AuthProject/myapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render,redirect
 from .forms import *
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+        unm=request.POST['username']
+        pas=request.POST['password']
+
+        user=usersignup.objects.filter(username=unm,password=pas)
+        if user: #true
+            print("Login successfull!")
+            request.session['user']=unm #session create
+            return redirect('home')
+        else:
+            print("Error!Login faild....Try again")
     return render(request,'index.html')
 
 def signup(request):
@@ -15,3 +27,11 @@ def signup(request):
         else:
             print(newuser.errors)
     return render(request,'signup.html')
+
+def home(request):
+    user=request.session.get('user')
+    return render(request,'home.html',{'user':user})
+
+def userlogout(request):
+    logout(request)
+    return redirect('/')
